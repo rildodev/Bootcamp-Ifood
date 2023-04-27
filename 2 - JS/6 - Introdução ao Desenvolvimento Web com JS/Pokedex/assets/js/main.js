9159b8e02412a3caa61718f21424da00ac0ecae5
rildodev <rildombs@gmail.com>
2023-04-27 20:02:49 -0300

    Primeira requisição da API

--- 2 - JS/6 - Introdução ao Desenvolvimento Web com JS/Pokedex/assets/js/main.js
+++ 2 - JS/6 - Introdução ao Desenvolvimento Web com JS/Pokedex/assets/js/main.js
@@ -1,4 +1,11 @@
-const btn = document.getElementById("btnTeste");
-btn.addEventListener("click", function () {
-  alert("Você clicou no botão");
-});
+
+
+const offset = 0;
+const limit = 0;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+/* requisição da API */
+fetch(url).then(function (response) {
+  console.log(response)
+})
