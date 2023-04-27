88da5a8600f2ddcea7c2f3354f8d3f3b4926bb2d
rildodev <rildombs@gmail.com>
2023-04-27 20:13:24 -0300

    Manipulando o resultado da requisição através de uma promise

--- 2 - JS/6 - Introdução ao Desenvolvimento Web com JS/Pokedex/assets/js/main.js
+++ 2 - JS/6 - Introdução ao Desenvolvimento Web com JS/Pokedex/assets/js/main.js
@@ -1,11 +1,9 @@
-
-
 const offset = 0;
-const limit = 0;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 /* requisição da API */
-fetch(url).then(function (response) {
-  console.log(response)
-})
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.error(error));
