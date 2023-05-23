1272c9b1b27347cab1a77ddbc5ca600f4a8a1319
rildodev <rildombs@gmail.com>
2023-05-23 20:41:35 -0300

    Construindo o build do seu component

--- 3 - Angular/1 - Trabalhando com Web Components no Front-end/Desafio-01/assets/js/cardNews.js
+++ 3 - Angular/1 - Trabalhando com Web Components no Front-end/Desafio-01/assets/js/cardNews.js
@@ -13,8 +13,20 @@ class cardNew extends HTMLElement {
     const cardLeft = document.createElement("div");
     cardLeft.setAttribute("class", "card_left");
 
+    const autor = document.createElement("span");
+    const linkTitle = document.createElement("a");
+    const newsContent = document.createElement("p");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
     const cardRight = document.createElement("div");
     cardRight.setAttribute("class", "card_right");
+
+    const newsImage = document.createElement("img");
+
+    cardRight.appendChild(newsImage);
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight);
