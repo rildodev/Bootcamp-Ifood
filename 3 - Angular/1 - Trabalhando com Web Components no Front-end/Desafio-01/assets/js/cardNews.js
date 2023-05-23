651fe47c6fea39ec4fd2f18b0b838a730ce54ae4
rildodev <rildombs@gmail.com>
2023-05-23 20:35:06 -0300

    Setando Propriedades de classes

--- 3 - Angular/1 - Trabalhando com Web Components no Front-end/Desafio-01/assets/js/cardNews.js
+++ 3 - Angular/1 - Trabalhando com Web Components no Front-end/Desafio-01/assets/js/cardNews.js
@@ -3,10 +3,24 @@ class cardNew extends HTMLElement {
     super();
 
     const shadow = this.attachShadow({ mode: "open" });
-
-    shadow.innerHTML = this.get;
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.style());
   }
-  build() {}
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card_left");
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card_right");
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+  }
   style() {}
 }
 
